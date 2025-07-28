3f556ce1cd90010756ef076ac59b7de6838fb9aa
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-07-28 13:51:23 +0900

    Update hello.cs

--- src/hello.cs
+++ src/hello.cs
@@ -5,3 +5,16 @@ bool isBoiled = true;
 Console.WriteLine(name);
 Console.WriteLine("Age: " + age);
 Console.WriteLine("Is boiled? " + isBoiled);
+
+if (isBoiled)
+{
+    Console.WriteLine(name + " is ready to eat! 🤤");
+}
+else if (age < 8 && !isBoiled)
+{
+    Console.WriteLine("NO! yu can't eat " + name + "!");
+}
+else
+{
+    Console.WriteLine(name + " is not ready ~ 😣");
+}
