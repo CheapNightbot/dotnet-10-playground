d90545eb94780edac225f43e568174ec8aa20fdd
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-07-28 14:04:42 +0900

    Add while loop

--- src/hello.cs
+++ src/hello.cs
@@ -1,20 +1,30 @@
 string name = "Potato";
 int age = 5;
-bool isBoiled = true;
+bool isBoiled = false;
 
-Console.WriteLine(name);
+Console.WriteLine("Hello, World! Me " + name + " ~");
 Console.WriteLine("Age: " + age);
 Console.WriteLine("Is boiled? " + isBoiled);
 
-if (isBoiled)
+while (true)
 {
-    Console.WriteLine(name + " is ready to eat! 🤤");
-}
-else if (age < 8 && !isBoiled)
-{
-    Console.WriteLine("NO! yu can't eat " + name + "!");
-}
-else
-{
-    Console.WriteLine(name + " is not ready ~ 😣");
+
+    if (isBoiled)
+    {
+        Console.WriteLine(name + " is ready to eat! 🤤");
+        break;
+    }
+    else if (age > 8)
+    {
+        isBoiled = true;
+    }
+    else if (age < 8 && !isBoiled)
+    {
+        Console.WriteLine("NO! yu can't eat " + name + "!");
+    }
+    else
+    {
+        Console.WriteLine(name + " is not ready ~ 😣");
+    }
+    age++;
 }
