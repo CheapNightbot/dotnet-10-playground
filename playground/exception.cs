1758a48c9d985bbcd0739cd9837041ad219753d1
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-07-29 10:17:48 +0900

    Update exception.cs

--- playground/exception.cs
+++ playground/exception.cs
@@ -1,27 +1,43 @@
 Console.Write("What is your age?: ");
 
 string age = Console.ReadLine() ?? "";
+// try
+// {
+//     int userAge = int.Parse(age);
+//     if (userAge <= 0)
+//     {
+//         throw new InvalidAgeException($"Baka, how is your age '{age}' ?? (。_。)");
+//     }
+// }
+// catch (FormatException)
+// {
+//     Console.WriteLine($"'{age}' - huh ? (ｏ ‵-′)ノ”(ノ﹏<。) don't you know numbers ?!!!");
+// }
+// catch (InvalidAgeException ex)
+// {
+//     Console.WriteLine(ex.Message);
+// }
+// finally
+// {
+//     Console.WriteLine("~ (￣m￣）");
+// }
+
 try
 {
-    int userAge = int.Parse(age);
-    if (userAge <= 0)
+    if (!int.TryParse(age, out int userAge))
+    {
+        Console.WriteLine($"'{age}' - huh ? (ｏ ‵-′)ノ”(ノ﹏<。) don't you know numbers ?!!!");
+    }
+
+    else if (userAge <= 0)
     {
         throw new InvalidAgeException($"Baka, how is your age '{age}' ?? (。_。)");
     }
-}
-catch (FormatException)
-{
-    Console.WriteLine($"'{age}' - huh ? (ｏ ‵-′)ノ”(ノ﹏<。) don't you know numbers ?!!!");
 }
 catch (InvalidAgeException ex)
 {
     Console.WriteLine(ex.Message);
 }
-finally
-{
-    Console.WriteLine("~ (￣m￣）");
-}
-
 
 public class InvalidAgeException : Exception
 {
