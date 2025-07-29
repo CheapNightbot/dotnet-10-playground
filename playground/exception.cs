570bbd9d244daea96917c002a1ef410041eba0ec
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-07-29 10:11:44 +0900

    custom exceptions ~ (ｏ ‵-′)ノ”(ノ﹏<。)

--- playground/exception.cs
+++ playground/exception.cs
@@ -6,18 +6,26 @@ try
     int userAge = int.Parse(age);
     if (userAge <= 0)
     {
-        throw new Exception($"Baka, how is your age '{age}' ?? (。_。)");
+        throw new InvalidAgeException($"Baka, how is your age '{age}' ?? (。_。)");
     }
 }
 catch (FormatException)
 {
     Console.WriteLine($"'{age}' - huh ? (ｏ ‵-′)ノ”(ノ﹏<。) don't you know numbers ?!!!");
 }
-catch (Exception ex)
+catch (InvalidAgeException ex)
 {
     Console.WriteLine(ex.Message);
 }
 finally
 {
     Console.WriteLine("~ (￣m￣）");
+}
+
+
+public class InvalidAgeException : Exception
+{
+    public InvalidAgeException() { }
+    public InvalidAgeException(string message) : base(message) { }
+    public InvalidAgeException(string message, Exception inner) : base(message, inner) { }
 }
